85977f9a71fba146b83ebb00388e066f57c87f0d
bienvennu <83087732+bienvennu@users.noreply.github.com>
2021-05-13 15:52:33 +0100

    Add files via upload

---
+++ data/Mairie_4.js
@@ -0,0 +1,8 @@
+var json_Mairie_4 = {
+"type": "FeatureCollection",
+"name": "Mairie_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Hôtel de ville de Bafia" }, "geometry": { "type": "Point", "coordinates": [ 11.231050124178548, 4.750705013694445 ] } }
+]
+}
